a80d6a09cd8ce03af374ff0ca8320ac29bea46ca
Gwalczak91 <gwalczak91@gmail.com>
2018-02-20 17:44:31 +0100

    Add home page and first jsx site

--- js/app.jsx
+++ js/app.jsx
@@ -2,7 +2,90 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { connect, Provider } from 'react-redux';
+import {HashRouter, Route, Redirect, Link, Switch, NavLink} from 'react-router-dom';
+import img from '../img/under18.jpeg'
+require("../scss/main.scss");
 
+
+class App extends React.Component {
+  constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <HashRouter>
+                <div>
+                    <Switch>
+                        <Route exact path="/" component={Start}></Route>
+                        <Route exact path="/adult" component={Adult}></Route>
+                        <Route exact path="/young" component={Young}></Route>
+                    </Switch>
+                </div>
+            </HashRouter>
+
+        )
+    }
+}
+
+class Start extends React.Component {
+  constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Great Casino</h1>
+                <p>Aby wejść na strone trzeba mieć ukonczone 18 lat</p>
+                <button><NavLink  exact to="/adult">Mam skończone 18 lat</NavLink></button>
+                <button><NavLink  exact to="/young">Mam poniżej 18 lat</NavLink></button>
+
+            </div>
+        )
+    }
+}
+
+
+class Adult extends React.Component {
+  constructor(props){
+        super(props);
+        this.state = {
+            option: ""
+        }
+    }
+
+    render(){
+          return (
+              <HashRouter>
+                  <div>
+                      <Switch>
+                          <Route exact path="/adult/" component={Adult}></Route>
+                          <Route exact path="/young/" component={Young}></Route>
+                      </Switch>
+                  </div>
+              </HashRouter>
+          )
+
+    }
+}
+
+class Young extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+                <div>
+                    <img src={img}></img>
+                    <h1>Wymagane posiadanie 18 lat</h1>
+                </div>
+            </div>
+        )
+    }
+}
 
 
 
@@ -13,7 +96,7 @@ import { connect, Provider } from 'react-redux';
 
 document.addEventListener('DOMContentLoaded', function(){
     ReactDOM.render(
-        <h1>Hello, World!</h1>,
+        <App />,
         document.getElementById('app')
     );
 });
