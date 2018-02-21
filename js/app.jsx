b771c7cde2043d6c2d5ff04eba6d415be9d4ef06
Gwalczak91 <gwalczak91@gmail.com>
2018-02-21 12:21:43 +0100

    Add to rulette site first view and events

--- js/app.jsx
+++ js/app.jsx
@@ -48,6 +48,23 @@ class Start extends React.Component {
     }
 }
 
+class Young extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+                <div>
+                    <img src={img}></img>
+                    <h1>Wymagane posiadanie 18 lat</h1>
+                </div>
+            </div>
+        )
+    }
+}
+
 class Adult extends React.Component {
   constructor(props){
         super(props);
@@ -98,17 +115,216 @@ class Adult extends React.Component {
 class Rulette extends React.Component {
   constructor(props){
         super(props);
+        this.state ={
+            kwota: 4000,
+            row1: [1,2,3,4,5,6,7,8,9,10,11,12],
+            row2: [13,14,15,16,17,18,19,20,21,22,23,24],
+            row3: [25,26,27,28,29,30,31,32,33,34,35,36],
+            randomNum: 0,
+            disable: false,
+            betSpace: "",
+            bet: 0
+
+        }
     }
 
+    randomizer = () => {
+      this.setState({
+          randomNum: Math.floor(Math.random()* 37),
+          disable: true
+      })
+    };
+
+    handleNumber = (e) => {
+        let addBet = prompt("Ile chcesz obstawić");
+        console.log(e.currentTarget);
+        console.log(Number(addBet));
+        this.setState({
+            kwota: this.state.kwota - addBet,
+            betSpace: e.currentTarget.innerText,
+            bet: addBet
+        })
+    };
+
+    checkResult = () => {
+        this.setState({
+            disable: false
+        })
+    };
+
+
     render(){
+
+      let firstRow = this.state.row1.map((e,i) => {
+          return <td onClick={this.handleNumber} key={i}>{e}</td>
+      });
+
+      let secondRow = this.state.row2.map((e,i) => {
+          return <td onClick={this.handleNumber} key={i}>{e}</td>
+      });
+
+      let thirdRow = this.state.row3.map((e,i) => {
+          return <td onClick={this.handleNumber} key={i}>{e}</td>
+      });
+
         return(
-            <div>
-                Witaj na rulettce
+            <div className="HolyGrail">
+                <Header>
+                    <h1>Moja strona</h1>
+                </Header>
+                <Container>
+                    <Content>
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        {firstRow}
+                                    </tr>
+                                    <tr>
+                                        {secondRow}
+                                    </tr>
+                                    <tr>
+                                        {thirdRow}
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <button disabled={this.state.disable} onClick={this.randomizer}>Losuj</button>
+                            <h1>{this.state.randomNum}</h1>
+                            <button onClick={this.checkResult}>Sprawdź</button>
+                    </Content>
+                    <Menu>
+                        <h1>Zasady gry w ruletke:</h1>
+                    </Menu>
+                    <Ad>
+                        <p>Plansza Gracza</p>
+                        <p>Twoja kwota to: {this.state.kwota}</p>
+                        <p>Postawiłeś: {this.state.bet} zł na pole {this.state.betSpace}</p>
+                    </Ad>
+                </Container>
+                <Footer>
+
+                </Footer>
             </div>
 
         )
     }
 }
+
+class Header extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+                {this.props.children}
+            </div>
+
+        )
+    }
+}
+
+class Content extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div className="HolyGrail-content">
+                {this.props.children}
+            </div>
+
+        )
+    }
+}
+
+class Menu extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div className="HolyGrail-nav">
+                {this.props.children}
+            </div>
+
+        )
+    }
+}
+
+class Ad extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+                {this.props.children}
+            </div>
+
+        )
+    }
+}
+
+class Footer extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+                {this.props.children}
+            </div>
+
+        )
+    }
+}
+
+class Container extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div className="HolyGrail-body">
+                {this.props.children}
+            </div>
+
+        )
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class DiceGame extends React.Component { // do zrobienia później
   constructor(props){
@@ -142,22 +358,6 @@ class BlackJack extends React.Component {   // do zrobienia później
 
 
 
-class Young extends React.Component {
-    constructor(props){
-        super(props);
-    }
-
-    render(){
-        return(
-            <div>
-                <div>
-                    <img src={img}></img>
-                    <h1>Wymagane posiadanie 18 lat</h1>
-                </div>
-            </div>
-        )
-    }
-}
 
 
 
