28f41c12dfa3a8191977a2a97b7f34e5e451fc87
Gwalczak91 <gwalczak91@gmail.com>
2018-02-23 09:59:46 +0100

    Add style to gameover site

--- js/gameover.jsx
+++ js/gameover.jsx
@@ -1,20 +1,34 @@
 import React from "react";
+import {HashRouter, Route, Redirect, Link, Switch, NavLink} from 'react-router-dom';
 
 class GameOver extends React.Component {
     constructor(props){
         super(props);
+        this.state = {
+            return: false
+        }
     }
+
+    changeButton = () => {
+        this.setState({
+            return: true
+        })
+    };
 
     render(){
-        return(
-            <div>
-                Skonczyły sie srodki
-            </div>
-
-        )
+        if(this.state.return === false) {
+            return (
+                <div className="home">
+                    <div style={{textAlign: "center"}}>
+                        <h1>Skończyły sie środki</h1>
+                        <button onClick={this.changeButton} style={{fontSize: "16px", marginTop: "20px"}}>Powrót do strony wyboru gier</button>
+                    </div>
+                </div>
+            )
+        }else {
+            return <Redirect to="/adult"/>
+        }
     }
 }
-
-
 
 export default GameOver;
