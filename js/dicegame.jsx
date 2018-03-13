46b3acad6789b7b3e1d8ed3f085ffd12ee67faab
Gwalczak91 <gwalczak91@gmail.com>
2018-03-13 11:34:49 +0100

    Add randomizer function to dice site

--- js/dicegame.jsx
+++ js/dicegame.jsx
@@ -10,15 +10,35 @@ class DiceGame extends React.Component { // do zrobienia później
         }
     }
 
+
+    randomizer = () => {
+
+      this.rando = setInterval(() => {
+          this.setState({
+              diceOne: Math.floor( Math.random() * ( 6 - 1 + 1 ) + 1 ),
+              diceTwo: Math.floor( Math.random() * ( 6 - 1 + 1 ) + 1 ),
+          })
+      }, 250);
+
+      this.timer = setTimeout(() => {
+          clearInterval(this.rando);
+      },2500);
+
+    };
+
     render(){
+
+        let diceSum = Number(this.state.diceOne) + Number(this.state.diceTwo)
+
         return(
             <div className="homeDice">
                 <h1>Kości</h1>
-                <button>Rzuć</button>
+                <button onClick={this.randomizer}>Rzuć</button>
                 <div className="dice-contener">
                     <div className="dice">{this.state.diceOne}</div>
                     <div className="dice">{this.state.diceTwo}</div>
                 </div>
+                <p>Wynik rzutu = {diceSum} </p>
             </div>
         )
     }
