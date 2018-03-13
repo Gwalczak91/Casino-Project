c5856fb11b58432f476231821eb067abbafacdb1
Gwalczak91 <gwalczak91@gmail.com>
2018-03-13 11:03:20 +0100

    Add dice to dices site

--- js/dicegame.jsx
+++ js/dicegame.jsx
@@ -4,12 +4,21 @@ import React from "react";
 class DiceGame extends React.Component { // do zrobienia później
     constructor(props){
         super(props);
+        this.state = {
+            diceOne: 1,
+            diceTwo: 1
+        }
     }
 
     render(){
         return(
             <div className="homeDice">
                 <h1>Kości</h1>
+                <button>Rzuć</button>
+                <div className="dice-contener">
+                    <div className="dice">{this.state.diceOne}</div>
+                    <div className="dice">{this.state.diceTwo}</div>
+                </div>
             </div>
         )
     }
